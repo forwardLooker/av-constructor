0bc040f0e95283c7e16bb108ab8ad03237aaab07
Anvar_Khakimov <pater@spark-mail.ru>
2023-06-19 09:26:52 +0300

    19/06 - grid бордеры поправил

--- dev/V/av-grid.js
+++ dev/V/av-grid.js
@@ -12,6 +12,26 @@ export class AVGrid extends AVElement {
         padding: 0 1.5em;
         box-shadow: 0 5px 10px 0 rgb(0 0 0 / 20%);
       }
+      .column:first-of-type .grid-header-cell {
+        border: 1px solid black;
+      }
+      .column:first-of-type .grid-cell {
+        border-right: 1px solid black;
+        border-left: 1px solid black;
+        border-bottom: 1px solid black;
+      }
+      .column:not(:first-of-type) .grid-header-cell {
+        border-right: 1px solid black;
+        border-top: 1px solid black;
+        border-bottom: 1px solid black;
+      }
+      .column:not(:first-of-type) .grid-cell {
+        border-right: 1px solid black;
+        border-bottom: 1px solid black;
+      }
+      .grid-header-cell {
+        text-align: center;
+      }
     `;
   }
 
@@ -28,10 +48,10 @@ export class AVGrid extends AVElement {
     return html`
       <div class="row">
           ${this.repeat(this.columns, c => c.name, c => html`
-              <div class="column flex-1">
-                  <div class="grid-header-cell border">${c.name}</div>
+              <div class="column col flex-1">
+                  <div class="grid-header-cell pad-8">${c.name}</div>
                   ${this.repeat(this.items, i => i.id, i => html`
-                      <div class="grid-cell border">${i[c.name]}</div>
+                      <div class="grid-cell pad-8">${i[c.name]}</div>
                   ` )}
               </div>
           `)}
