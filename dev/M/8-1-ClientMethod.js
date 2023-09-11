b1821599d55729084a64f2ae01600db8d781218b
Anvar_Khakimov <pater@spark-mail.ru>
2023-09-11 21:46:09 +0300

    Methods

---
+++ dev/M/8-1-ClientMethod.js
@@ -0,0 +1,7 @@
+import {Item} from './0-Item.js'
+
+export class ClientMethod extends Item {
+  constructor() {
+    super();
+  }
+};
---
+++ dev/M/8-3-VoiceMethod.js
@@ -0,0 +1,7 @@
+import {Item} from './0-Item.js'
+
+export class VoiceMethod extends Item {
+  constructor() {
+    super();
+  }
+};
