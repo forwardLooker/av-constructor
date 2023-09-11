5582f65ad8ad46ea6f4f83c3e9ce10dbd2f863a7
Anvar_Khakimov <pater@spark-mail.ru>
2023-09-12 00:03:42 +0300

    System Directory

---
+++ dev/M/System/0-Session.js
@@ -0,0 +1,7 @@
+import {Item} from './0-Item.js'
+
+export class Session extends Item {
+  constructor() {
+    super();
+  }
+};
---
+++ dev/M/System/3-FileStorage.js
@@ -0,0 +1,7 @@
+import {Item} from './0-Item.js'
+
+export class FileStorage extends Item {
+  constructor() {
+    super();
+  }
+};
