b8ea3e25f2557ba2c0f5e48347fca336dc1d66cf
Anvar_Khakimov <pater@spark-mail.ru>
2023-09-27 16:41:12 +0300

    DataType -> FieldValue комменты

--- dev/M/5-Field.js
+++ dev/M/5-Field.js
@@ -4,4 +4,7 @@ export class Field extends Item {
   constructor() {
     super();
   }
+  // TODO содержит label, FieldValue, ViewItem управляющий FieldValue
+  // TODO управляет значением, вычислими значениями, метаданными, валидацией
+  // TODO как идея Статики Филда или любого айтема, как декораторы- хайер-ордер компоненты
 };
