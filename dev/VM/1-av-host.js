27a30952d3feee48182a8e966b7ea51640297725
Anvar_Khakimov <pater@spark-mail.ru>
2023-10-04 12:14:43 +0300

    Перед переносом на реакт

--- dev/VM/1-av-host.js
+++ dev/VM/1-av-host.js
@@ -14,45 +14,6 @@ import '../V/av-property-grid.js';
 
 import {Host} from'../M/1-Host.js';
 
-// const config = {
-//   version: 1,
-//   id: 'main',
-//   name: 'Мой Хост',
-//   itemType: 'host',
-//   items: [
-//     {version: 1, id: 'system', name: 'System', itemType: 'domain', items: [
-//         {version: 1, id: 'users', name: 'Пользователи', itemType: 'class', items: []}
-//       ]},
-//     {version: 1, id: 'workspace', name: 'Workspace', itemType: 'domain', items: [
-//         {version: 1, id: 'dictionaries', name: 'Справочники', itemType: 'domain', items: [
-//             {version: 1, id: '1', name: 'Физ. лица', itemType: 'class', items: []}
-//           ]},
-//         {version: 1, id: 'documents', name: 'Документы', itemType: 'domain', items: [
-//             {version: 1, id: '1', name: 'Приходная налкадная', itemType: 'class', items: []}
-//           ]},
-//         {version: 1, id: 'services', name: 'Сервисы', itemType: 'domain', items: [
-//             {version: 1, id: '1', name: 'Журнал учёта', itemType: 'domain', items: [
-//                 {version: 1, id: '1', name: 'Журнал учёта', itemType: 'class', items: []},
-//                 {version: 1, id: '2', name: 'Проводки', itemType: 'class', items: []}
-//               ]}
-//           ]},
-//         {version: 1, id: 'solutions', name: 'Решения', itemType: 'domain', items: []},
-//         {version: 1, id: 'workplaces', name: 'Рабочие места', itemType: 'domain', items: []},
-//       ]},
-//   ],
-// }
-
-const userObjects = [
-  {version: 1, id: '1', name: 'Admin', itemType: 'object', class: 'users', email:'me@mail.ru'},
-  {version: 1, id: '2', name: 'User1', itemType: 'object', class: 'users', email:'myself@mail.ru'},
-  {version: 1, id: '3', name: 'Account2', itemType: 'object', class: 'users', email:'I@mail.ru'},
-];
-const userFieldDescriptors = [
-  {name: 'id', itemType: 'field'},
-  {name: 'name', itemType: 'field'},
-  {name: 'email', itemType: 'field'}
-];
-
 export class AVHost extends AVItem {
   static get styles() {
     return css`
