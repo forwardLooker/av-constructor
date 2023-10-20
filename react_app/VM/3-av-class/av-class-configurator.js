1350a6b6f8bdece4528befe7e20c241c9e7e569a
Anvar_Khakimov <pater@spark-mail.ru>
2023-10-20 16:27:48 +0300

    Перенос на реакт

--- react_app/VM/3-av-class/av-class-configurator.js
+++ react_app/VM/3-av-class/av-class-configurator.js
@@ -2,7 +2,100 @@ import {html, css, AVItem} from '../0-AVItem.js';
 
 import '../../V/AVTree.jsx';
 
-export class AvClassConfigurator extends AVItem {
+export class AVClassConfigurator extends AVItem {
+  static defaultProps = {
+    classItem: null,
+    onSavedFunc: this.noop,
+  }
+  state = {
+    fieldDescriptors: [],
+    _newFieldDescriptors: [],
+    selectedFieldDescriptor: null,
+  }
+  fieldDescriptorProperties = [
+    {name: 'label'},
+    {name: 'dataType', dataType: 'string', variant: 'select', valuesList: 'string,number,boolean,array,object, link, include-link'},
+    {name: 'variant'},
+    {name: 'valuesList'},
+    {name: 'defaultValue'},
+    {name: 'isHidden', dataType: 'boolean'},
+    {name: 'isComputed', dataType: 'boolean'},
+    {name: 'computeFunction'}
+  ];
+
+  render() {
+    return (
+      <div className="col space-between flex-1">
+        <div className="col flex-1">
+          <AVTextHeader class="margin-top-8">Fields:</AVTextHeader>
+          <div>
+            <AVButton onClick={this._addField}>Добавить поле</AVButton>
+          </div>
+          <div className="row flex-1 margin-top-8">
+            <AVTree
+              class="fields-tree border"
+              items={this.state._newFieldDescriptors}
+              onItemSelectFunc={item => this.setState({selectedFieldDescriptor: item})}
+              onItemContextMenuFunc={this._onTreeItemContextMenu}
+            ></AVTree>
+            <AVPropertyGrid
+              class="flex-1 margin-left-8 border"
+              inspectedItem={this.state.selectedFieldDescriptor}
+              propertyItems={this.fieldDescriptorProperties}
+            ></AVPropertyGrid>
+          </div>
+        </div>
+        <div className="row justify-end">
+          <AVButton OnClick={this._saveFieldDescriptors}>Сохранить</AVButton>
+        </div>
+      </div>
+    )
+  }
+
+  async componentDidMount() {
+    if (this.props.classItem) {
+      const fieldDescriptors = await this.props.classItem.getFieldDescriptors();
+      this.setState({fieldDescriptors});
+    }
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    if (this.state.fieldDescriptors !== prevState.fieldDescriptors) {
+      this.setState({_newFieldDescriptors: this.deepClone(this.state.fieldDescriptors)});
+    }
+  }
+
+  _onTreeItemContextMenu = async (e, item) => {
+    const menuChoice = await this.showContextMenu(e, ['Добавить вложенное поле', 'Удалить поле']);
+    if (menuChoice === 'Добавить вложенное поле') {
+      const fieldName = await this.showDialog({text: 'Введите название поля', input: 'name'});
+      if (fieldName) {
+        if (this.notEmpty(item.items) && item.items.every(f => f.name !== fieldName)) {
+          item.items.push({name: fieldName, label: fieldName, dataType: 'string'})
+        }
+        if (this.isEmpty(item.items)) {
+          item.items = [{name: fieldName, label: fieldName, dataType: 'string'}];
+        }
+        this._newFieldDescriptors = [...this._newFieldDescriptors];
+      }
+    }
+  }
+
+  _addField = async () => {
+    const fieldName = await this.showDialog({text: 'Введите название поля', input: 'name'});
+    if (fieldName && this._newFieldDescriptors.every(f => f.name !== fieldName)) {
+      const field = {name: fieldName, label: fieldName, dataType: 'string'};
+      this._newFieldDescriptors = [...this._newFieldDescriptors, field];
+    }
+  }
+
+  _saveFieldDescriptors = async () => {
+    await this.classItem.saveFieldDescriptors(this._newFieldDescriptors);
+    this.onSaveFunc();
+  }
+}
+
+export class AvClassConfigurator2 extends AVItem {
   static get styles() {
     return css`
       :host {
@@ -52,26 +145,26 @@ export class AvClassConfigurator extends AVItem {
     return html`
         <div class="col space-between flex-1">
           <div class="col flex-1">
-            <av-text-header class="margin-top-8">Fields:</av-text-header>
+            <AVTextHeader class="margin-top-8">Fields:</AVTextHeader>
             <div>
-              <av-button @click="${this._addField}">Добавить поле</av-button>
+              <AVButton onClick={this._addField}>Добавить поле</AVButton>
             </div>
             <div class="row flex-1 margin-top-8">
-              <av-tree
+              <AVTree
                 class="fields-tree border"
-                .items="${this._newFieldDescriptors}"
-                .onItemSelectFunc="${item => this.selectedField = item}"
-                .onItemContextMenuFunc="${this._onTreeItemContextMenu}"
-              ></av-tree>
-              <av-property-grid
+                items={this._newFieldDescriptors}
+                onItemSelectFunc={item => this.selectedField = item}
+                onItemContextMenuFunc={this._onTreeItemContextMenu}
+              ></AVTree>
+              <AVPropertyGid
                 class="flex-1 margin-left-8 border"
-                .inspectedItem="${this.selectedField}"
-                .propertyItems="${this.prGridItems}"
-              ></av-property-grid>
+                inspectedItem={this.selectedField}
+                propertyItems={this.prGridItems}
+              ></AVPropertyGid>
             </div>
           </div>
           <div class="row justify-end">
-            <av-button @click="${this._saveFieldDescriptors}">Сохранить</av-button>
+            <AVButton OnClick={this._saveFieldDescriptors}>Сохранить</AVButton>
           </div>
         </div>
     `
@@ -117,4 +210,4 @@ export class AvClassConfigurator extends AVItem {
   }
 }
 
-window.customElements.define('av-class-configurator', AvClassConfigurator);
+window.customElements.define('av-class-configurator', AvClassConfigurator2);
