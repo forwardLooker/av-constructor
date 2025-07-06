14166fb7dab00c40ad3d83c429167b1fd9a7238b
Anvar_Khakimov <pater@spark-mail.ru>
2025-07-06 23:26:31 +0300

    Валидация page2

--- react_app/Classes/gazprombank-page2.js
+++ react_app/Classes/gazprombank-page2.js
@@ -3,16 +3,7 @@ export default class {
   static id = 'HtL6UraG8Iz3L3z51f5f';
   static name = 'Газпромбанк (стр.2)';
   static Host; // инициализируется в момент соединения с классом
-  // static onComponentDidMount = async ($objectDocument) => {
-  //   if (!$objectDocument.state._newData['Я налоговый резидент только РФ']) {
-  //     if (!$objectDocument.state.presentationGroupsHidden.includes('nalogResidentNo')) {
-  //       $objectDocument.state.presentationGroupsHidden.push('nalogResidentNo');
-  //       $objectDocument.forceUpdate();
-  //     }
-  //   }
-  //   $objectDocument.state.presentationGroupsHidden.push('confirmSMS');
-  //   $objectDocument.forceUpdate();
-  // }
+  
   static on_newDataChange = async ({ $objectDocument, fieldItemName, value }) => {
     if (fieldItemName === 'Фактический адрес проживания совпадает с адресом регистрации') {
       if (value === 'Нет') {
@@ -30,95 +21,89 @@ export default class {
       }
     }
   }
-  // static methods = {
-  //   'Подтвердить данные': async ($objectDocument) => {
-  //     console.log('Подтвердить данные успех', this.Host);
-  //     let stopNavigate;
-  //     // проверки полей на наличие
-  //     if (!$objectDocument['fieldRef_Фамилия Имя Отчество'].state._value) {
-  //       $objectDocument['fieldRef_Фамилия Имя Отчество'].setState({
-  //         isInvalidState: true,
-  //         isRequiredMessageRendered: true,
-  //       });
-  //       stopNavigate = true
-  //     }
-  //     if (!$objectDocument['fieldRef_Мобильный телефон'].state._value) {
-  //       $objectDocument['fieldRef_Мобильный телефон'].setState({
-  //         isInvalidState: true,
-  //         isRequiredMessageRendered: true,
-  //       });
-  //       stopNavigate = true
-  //     }
-  //     if (!$objectDocument['fieldRef_Email'].state._value) {
-  //       $objectDocument['fieldRef_Email'].setState({
-  //         isInvalidState: true,
-  //         isRequiredMessageRendered: true,
-  //       });
-  //       stopNavigate = true
-  //     }
-  //     // проверки полей на валидность
-  //     if ($objectDocument['fieldRef_Я налоговый резидент только РФ'].state._value !== 'Да') {
-  //       $objectDocument['fieldRef_Я налоговый резидент только РФ'].setState({
-  //         isInvalidState: true,
-  //         isInvalidMessageRendered: true,
-  //         invalidMessage: 'Выберите один из вариантов',
-  //       });
-  //       stopNavigate = true
-  //     }
-  //     if ($objectDocument['fieldRef_Мобильный телефон'].state.isInvalidState) {
-  //       stopNavigate = true
-  //     }
-  //     if ($objectDocument['fieldRef_Email'].state.isInvalidState) {
-  //       stopNavigate = true
-  //     }
-  //     // проверки чекбоксов на отмечено
-  //     if (!$objectDocument['fieldRef_Я подтверждаю заявку и даю согласие на обработку персональных данных'].state._value) {
-  //       $objectDocument['fieldRef_Я подтверждаю заявку и даю согласие на обработку персональных данных'].setState({
-  //         isInvalidState: true,
-  //         isInvalidMessageRendered: true,
-  //         invalidMessage: 'Заявка не может быть обработана без вашего согласия',
-  //       });
-  //       stopNavigate = true
-  //     }
-  //     if (!$objectDocument['fieldRef_Я даю согласие на передачу третьим лицам персональных данных'].state._value) {
-  //       $objectDocument['fieldRef_Я даю согласие на передачу третьим лицам персональных данных'].setState({
-  //         isInvalidState: true,
-  //         isInvalidMessageRendered: true,
-  //         invalidMessage: 'Заявка не может быть обработана без вашего согласия',
-  //       });
-  //       stopNavigate = true
-  //     }
-  //     if (!$objectDocument['fieldRef_Я даю согласие на получение Банком информации из БКИ'].state._value) {
-  //       $objectDocument['fieldRef_Я даю согласие на получение Банком информации из БКИ'].setState({
-  //         isInvalidState: true,
-  //         isInvalidMessageRendered: true,
-  //         invalidMessage: 'Заявка не может быть обработана без вашего согласия',
-  //       });
-  //       stopNavigate = true
-  //     }
+  
+  static methods = {
+    'Далее': async ($objectDocument) => {
+      console.log('Далее', this.Host);
+      let stopNavigate;
+      // проверки полей на наличие
+      if (!$objectDocument['fieldRef_Серия и номер'].state._value) {
+        $objectDocument['fieldRef_Серия и номер'].setState({
+          isInvalidState: true,
+          isRequiredMessageRendered: true,
+        });
+        stopNavigate = true
+      }
+      if (!$objectDocument['fieldRef_Дата выдачи'].state._value) {
+        $objectDocument['fieldRef_Дата выдачи'].setState({
+          isInvalidState: true,
+          isRequiredMessageRendered: true,
+        });
+        stopNavigate = true
+      }
+      if (!$objectDocument['fieldRef_Код подразделения'].state._value) {
+        $objectDocument['fieldRef_Код подразделения'].setState({
+          isInvalidState: true,
+          isRequiredMessageRendered: true,
+        });
+        stopNavigate = true
+      }
+      if (!$objectDocument['fieldRef_Кем выдан'].state._value) {
+        $objectDocument['fieldRef_Кем выдан'].setState({
+          isInvalidState: true,
+          isRequiredMessageRendered: true,
+        });
+        stopNavigate = true
+      }
+      if (!$objectDocument['fieldRef_Дата рождения'].state._value) {
+        $objectDocument['fieldRef_Дата рождения'].setState({
+          isInvalidState: true,
+          isRequiredMessageRendered: true,
+        });
+        stopNavigate = true
+      }
+      if (!$objectDocument['fieldRef_Место рождения'].state._value) {
+        $objectDocument['fieldRef_Место рождения'].setState({
+          isInvalidState: true,
+          isRequiredMessageRendered: true,
+        });
+        stopNavigate = true
+      }
+      if (!$objectDocument['fieldRef_Семейное положение'].state._value) {
+        $objectDocument['fieldRef_Семейное положение'].setState({
+          isInvalidState: true,
+          isRequiredMessageRendered: true,
+        });
+        stopNavigate = true
+      }
 
+      // проверки полей на валидность, 3 поля простые поэтому немного избыточно
+      if ($objectDocument['fieldRef_Серия и номер'].state.isInvalidState) {
+        stopNavigate = true
+      }
+      if ($objectDocument['fieldRef_Дата выдачи'].state.isInvalidState) {
+        stopNavigate = true
+      }
+      if ($objectDocument['fieldRef_Код подразделения'].state.isInvalidState) {
+        stopNavigate = true
+      }
+      if ($objectDocument['fieldRef_Кем выдан'].state.isInvalidState) {
+        stopNavigate = true
+      }
+      if ($objectDocument['fieldRef_Дата рождения'].state.isInvalidState) {
+        stopNavigate = true
+      }
+      if ($objectDocument['fieldRef_Место рождения'].state.isInvalidState) {
+        stopNavigate = true
+      }
+      if ($objectDocument['fieldRef_Семейное положение'].state.isInvalidState) {
+        stopNavigate = true
+      }
 
-  //     if (!stopNavigate) {
-  //       // render SMS
-  //       if (!$objectDocument.state.presentationGroupsHidden.includes('confirmData')) {
-  //         $objectDocument.state.presentationGroupsHidden.push('confirmData');
-  //       }
-  //       if ($objectDocument.state.presentationGroupsHidden.includes('confirmSMS')) {
-  //         const idxToRemove = $objectDocument.state.presentationGroupsHidden.findIndex(prGr => prGr === 'confirmSMS');
-  //         $objectDocument.state.presentationGroupsHidden.splice(idxToRemove, 1);
-  //       }
-  //       $objectDocument.forceUpdate();
-
-  //       // this.Host.navigate('/gaz2');
-  //     }
-  //     // redirect('/gaz2');
-  //     // $objectDocument.closeWithoutSave();
-  //     // this.Host.$hostElement.setState({ selectedTreeItem: await this.Host.getClassByName('Сотрудники') })
-  //   },
-  //   'Подтвердить': async ($objectDocument) => {
-  //     if ($objectDocument.state._newData['Код из СМС'] === '1111') {
-  //       this.Host.navigate('/gaz2');
-  //     }
-  //   }
-  // };
+      if (!stopNavigate) {
+        $objectDocument.Host.gazCreditSecondPageData = $objectDocument.state._newData;
+        this.Host.navigate('/gaz3');
+      }
+    },
+  };
 }
