c6c29995b3576ba4de8b64d748fbb608c6d4ccca
Anvar_Khakimov <pater@spark-mail.ru>
2025-12-25 20:10:41 +0300

    Выравнивание кнопки по центру

--- react_app/VM/5-AVField.jsx
+++ react_app/VM/5-AVField.jsx
@@ -392,7 +392,7 @@ class AVFieldOriginal extends AVItem {
     }
     if (this.props.fieldItem.viewItemType === 'button') {
       return (
-        <div className='_av-field-viewItem-root flex-1 pad-8-0'
+        <div className='_av-field-viewItem-root flex-1 col justify-center align-baseline pad-8-0'
              style={this.props.style}
              ref={this.props.refOnRootDiv}
         >
