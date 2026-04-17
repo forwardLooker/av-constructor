1991b19c9fad67d9cd3a1794b7828ee877e54252
Anvar Khakimov <askhakimov@mail.ru>
2026-04-17 19:08:48 +0300

    шрифты подключил

--- webpack.config.js
+++ webpack.config.js
@@ -11,30 +11,30 @@ module.exports = {
     rules: [
       {test: /\.(jsx|js)$/, use: 'babel-loader'},
       { test: /\.css$/, use: ['style-loader', 'css-loader'] },
-      {
-        test: /\.(woff|woff2|eot|ttf|otf)$/i,
-        use: [
-          {
-            loader: 'url-loader',
-            options: {
-              limit: 8192, // Inline fonts smaller than 8kb
-              name: 'assets/fonts/[name].[hash:8].[ext]' // Fallback path
-            },
-          },
-        ],
-      },
       // {
       //   test: /\.(woff|woff2|eot|ttf|otf)$/i,
       //   use: [
       //     {
-      //       loader: 'file-loader',
+      //       loader: 'url-loader',
       //       options: {
-      //         name: '[name].[ext]',
-      //         outputPath: 'fonts', // optional: puts fonts in a subfolder
+      //         limit: 8192, // Inline fonts smaller than 8kb
+      //         name: 'assets/fonts/[name].[hash:8].[ext]' // Fallback path
       //       },
       //     },
       //   ],
-      // }
+      // },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/i,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              name: '[name].[ext]',
+              outputPath: 'fonts', // optional: puts fonts in a subfolder
+            },
+          },
+        ],
+      }
     ]
   },
   devServer: {
