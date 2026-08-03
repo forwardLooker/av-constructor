b13d297d8538fe078438db57e816ef2df2ff43da
Anvar Khakimov <askhakimov@mail.ru>
2026-08-03 15:27:49 +0300

    Button refactor

--- react_app/V/AVButton.jsx
+++ react_app/V/AVButton.jsx
@@ -9,20 +9,12 @@ export class AVButton extends AVElement {
       text-align: center;
       color: white;
       background-color: #1b1c1f;
-      border-color: #1b1c1f;
-      transition: 80ms cubic-bezier(0.33, 1, 0.68, 1);
-      transition-property: color,background-color,box-shadow,border-color;
       padding: 5px 16px;
       font-size: 14px;
-      font-weight: var(--base-text-weight-medium, 500);
-      line-height: 20px;
-      white-space: nowrap;
-      vertical-align: middle;
+      font-weight: 500;
       cursor: pointer;
       user-select: none;
-      border: 1px solid;
       border-radius: 8px;
-      appearance: none;
       
       &:hover {
         opacity: 0.90;
